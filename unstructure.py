1abf68eccd1d19b93750dfe4987f3042e5e4d68a
DeepakJaiz <120568685+DeepakJaiz@users.noreply.github.com>
2023-05-29 13:22:46 +0530

    Add files via upload

---
+++ unstructure.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from llama_index import download_loader
+import pytesseract
+
+UnstructuredReader = download_loader("UnstructuredReader")
+
+loader = UnstructuredReader()
+documents = loader.load_data(file=Path('./data/Pride-and-Prejudice.pdf'))
+print(documents)
+
+
