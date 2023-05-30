d362bdfa73fa6076d1ec3974e84322d6c6aeabc1
DeepakJaiz <120568685+DeepakJaiz@users.noreply.github.com>
2023-05-30 17:33:36 +0530

    Delete unstructure_pdf.py

--- unstructure_pdf.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from llama_index import download_loader
-
-UnstructuredReader = download_loader("UnstructuredReader")
-
-loader = UnstructuredReader()
-documents = loader.load_data(file=Path('./data/Pride-and-Prejudice.pdf'))
-
-print(documents)
